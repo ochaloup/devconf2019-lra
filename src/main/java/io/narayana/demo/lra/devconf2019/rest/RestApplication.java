5635320c31f98fd1d8d912be17ee9e2a1c499637
Ondra Chaloupka <ochaloup@redhat.com>
2019-01-21 13:00:53 +0100

    init commit

---
+++ src/main/java/io/narayana/demo/lra/devconf2019/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package io.narayana.demo.lra.devconf2019.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
